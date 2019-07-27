ee0befc75fc755a3e646ff20e2d173587d591a02
Dagmara Tomasik <Tomasik.Dagmara@outlook.com>
2019-07-27 11:57:38 +0200

    Add active link

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log('event');
 
   /* [DONE] remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    activeLink.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.post-content p.active');
 
-    for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
-    }
+  const activeArticles = document.querySelectorAll('.post-content p.active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
