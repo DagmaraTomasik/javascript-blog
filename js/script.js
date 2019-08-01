932ba2f8e095b54ef296d219b003841667a31a12
Dagmara Tomasik <Tomasik.Dagmara@outlook.com>
2019-08-01 14:54:36 +0200

    Add links tags

--- js/script.js
+++ js/script.js
@@ -91,27 +91,53 @@ generateTitleLinks();
 function generateTags(){
   /* find all articles */
 
+  const articles = document.querySelectorAll(optArticleSelector);
+
   /* START LOOP: for every article: */
+
+  for (let article of articles){
 
     /* find tags wrapper */
 
+    const tagLinks = article.querySelector(optArticleTagsSelector);
+
     /* make html variable with empty string */
+
+    let html = ' ';
 
     /* get tags from data-tags attribute */
 
+    const articleTags = article.getAttribute('data-tags');
+    console.log(articleTags);
+
     /* split tags into array */
+
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
 
     /* START LOOP: for each tag */
 
+    for (let tag of articleTagsArray){
+
       /* generate HTML of the link */
+
+      const linkHTML = '<li><a href="#tag-'+ tag +'">'+ tag +'</a></li>';
 
       /* add generated code to html variable */
 
+      html = html + linkHTML;
+
     /* END LOOP: for each tag */
+
+  }
 
     /* insert HTML of all the links into the tags wrapper */
 
+    tagLinks.insertAdjacentHTML('beforeend', html);
+
   /* END LOOP: for every article: */
+
+  }
 }
 
 const optArticleTagsSelector = '.post-tags .list';
