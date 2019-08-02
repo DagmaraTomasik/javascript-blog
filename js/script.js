85e3cf756a07f669fde4f62fd4245d60f6371952
Dagmara Tomasik <Tomasik.Dagmara@outlook.com>
2019-08-02 15:26:11 +0200

    Add calculate

--- js/script.js
+++ js/script.js
@@ -90,8 +90,42 @@ generateTitleLinks();
 
 const optArticleTagsSelector = '.post-tags .list';
 const optArticleTagsElementSelector = '.post-tags .list li';
+const optTagsListSelector = '.tags.list';
+const optCloudClassCount = '5';
+const optCloudClassPrefix = 'tag-size-';
+
+function calculateTagsParams(tags){
+
+  const params = {max: '0', min: '999999'}
+  console.log(params)
+
+  for(let tag in tags){
+  console.log(tag + ' is used ' + tags[tag] + ' times ');
+
+    if(tags[tag] > params.max){
+    params.max = tags[tag];
+    }
+    else if(tags[tag] < params.min){
+    params.min = tags[tag];
+    }
+  }
+  return params;
+}
+
+function calculateTagClass(count, params){
+
+  const normalizedCount = count - params.min;
+  const percentage = normalizedCount / normalizedMax;
+  const classNumber = Math.floor( percentage * (optCloudClassCount - 1) + 1 );
+
+}
 
 function generateTags(){
+
+  /* [NEW] create a new variable allTags with an empty object */
+
+  let allTags = {};
+
   /* find all articles */
 
   const articles = document.querySelectorAll(optArticleSelector);
@@ -130,9 +164,49 @@ function generateTags(){
 
       html = html + linkHTML;
 
+      /* [NEW] check if this link is NOT already in allTags */
+
+      if(!allTags.hasOwnProperty(tag)){
+
+        /* [NEW] add tag to allTags object */
+
+        allTags[tag]=1;
+      }else {
+        allTags[tag]++;
+      }
+
       /* END LOOP: for each tag */
 
     }
+
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector('.tags');
+
+  const tagsParams = calculateTagsParams(allTags);
+  console.log('tagsParams:', tagsParams);
+
+  /* [NEW] create variable for all links HTML code */
+
+  let allTagsHTML=' ';
+
+  /* [NEW] START LOOP: for each tak in allTags */
+
+  for(let tag in allTags){
+
+    const tagLinkHTML = '<li><a class="'+ optCloudClassPrefix + calculateTagClass(allTags[tag], tagsParams) +'" href ="#tag-'+ tag +'">' + tag + '</a>(' + allTags[tag] + ')</li> ';
+    console.log('tagLinkHTML:', tagLinkHTML);
+
+    /* [NEW] generate code of a link and add it to allTagsHTML */
+
+    allTagsHTML += tagLinkHTML;
+
+    /* [NEW] END LOOP: for each tag in allTags */
+
+    }
+
+    /* [NEW] add html from allTagsHTML to tagList */
+
+    tagList.innerHTML = allTagsHTML;
 
     /* insert HTML of all the links into the tags wrapper */
 
